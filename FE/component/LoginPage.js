05adaaf70b69f9ecb3c27a283848bb80abbd853a
jeongseungmin <taky0315@naver.com>
2023-05-07 00:20:31 +0900

    기능은 하나도 없지만, 카테고리 누를 경우 카테고리이름과, 사용할 카테고리 이미지 주소 넣는 화면으로 넘어가게 화면 구성해둠

--- FE/component/LoginPage.js
+++ FE/component/LoginPage.js
@@ -12,6 +12,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 function LoginPage(props){
+    //여기가 이제 + 버튼 관리하는 스테이트
     const [isModalVisible, setIsModalVisible] = useState(false);
     const handleOpenModal = () => {
         setIsModalVisible(true);
@@ -20,7 +21,24 @@ function LoginPage(props){
         setIsModalVisible(false);
     };
 
+    //카테고리 추가 버튼 관리 스테이트
+    const [isModalVisible2, setIsModalVisible2] = useState(false);
+    const handleOpenModal2 = () => {
+        setIsModalVisible(true);
+    };
+    const handleCloseModal2 = () => {
+        setIsModalVisible(false);
+    };
+
+
+
     const [page, changePage] = useState('My BuketList')
+    const images = [
+        'https://www.korea.kr/newsWeb/resources/temp/images/000074/img_01.jpg',
+        'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_2916,h_1944,f_auto/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/eltdn5sjc3mnkq2sg3z7/NZONE퀸스타운스카이다이빙-클룩KLOOK한국.jpg',
+        'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/03/089fb301-56e6-4169-b5c5-8efea295cdd6.jpg',
+        'https://media.istockphoto.com/id/1392044276/ko/벡터/폴더-아이콘-스톡-그림-파일-폴더-링-바인더-아이콘-컴퓨터-데스크톱-pc.jpg?s=1024x1024&w=is&k=20&c=ZELmyl9nCe2EpYZ7C5gMSMaqwg2HLq_zmpQI4fhutT8='
+    ];
 
     return(
         <View style={styles.container}>
@@ -49,16 +67,11 @@ function LoginPage(props){
             {/* Main에 카테고리 이미지 넣는 부분 */}
             <View style ={{ width : '100%', height : '60%'}}>
                 <ScrollView>
-                    {[1,2,3,4,5].map((i) =>(
-                        <View style ={{flexDirection : 'row', marginTop : 2}}>
+                    {images.map((item, index) =>(
+                        <View style ={{ margin : 3, alignItems : 'center'}}>
                             <Image
-                                source={require('../image/img.png')}
-                                style={{ width: '50%', height: 200, }}/>
-                            <View style ={{margin : 3}}></View>
-                            <Image
-                                source={require('../image/img.png')}
-                                style={{ width: '50%', height: 200 }}
-                            />
+                                source={{uri: item}}
+                                style={{ width: '70%', height: 200, }}/>
                         </View>
                     ))
 
@@ -68,11 +81,9 @@ function LoginPage(props){
 
             {/* 카테고리 추가 버튼*/}
             <View style={{height : '5%', width : '100%', flexDirection : 'row'}}>
-                    <TouchableOpacity style={{alignItems : 'center', width : '100%'}} onPress={()=>{
-                        props.navigation.navigate('')}
-                    }>
-                        <Text style = {{flex : 1, textAlign : 'center'}}>카테고리 추가</Text>
-                    </TouchableOpacity>
+                <TouchableHighlight style={{alignItems : 'center', width : '100%'}} onPress={handleOpenModal2}>
+                    <Text style ={{fontSize : 15}}>카테고리 추가하기</Text>
+                </TouchableHighlight>
             </View>
 
             {/* 아래 하단바 footer */}
@@ -143,6 +154,35 @@ function LoginPage(props){
                           </TouchableHighlight>
                       </View>
                    </View>
+                </View>
+            </Modal>
+
+
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={isModalVisible}
+                onRequestClose={handleCloseModal2}>
+                <View>
+                    <View style={{alignItems : 'center'}}>
+                        <View style ={{width : '100%', height : '20%'}}>
+                            <Text style ={{textAlign : 'center', fontSize : 30}}>새로운 카테고리 추가하기</Text>
+                        </View>
+                        <View style ={{width : '80%', height : '70%', backgroundColor : 'gray', borderRadius : 10}}>
+                            <Text style ={{textAlign : 'center'}}>카테고리 이름</Text>
+                            <TextInput placeholder="카테고리 이름 설정"></TextInput>
+                            <Text style ={{textAlign : 'center'}}>이미지 첨부</Text>
+                            <TextInput placeholder="이미지 주소 첨부하기"></TextInput>
+
+
+                        </View>
+                        <View>
+                            <TouchableHighlight onPress={handleCloseModal2} underlayColor ="red"
+                                                style={{width : '13%'}}>
+                                <Text>돌아가기</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
                 </View>
             </Modal>
 
