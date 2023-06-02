79841b570e5e0a2a551d8f4ad326e6a8562a7e5f
jeongseungmin <taky0315@naver.com>
2023-06-03 04:23:38 +0900

    tlqkf

--- FE/component/pageFolder/category/CategoryPage.js
+++ FE/component/pageFolder/category/CategoryPage.js
@@ -35,7 +35,7 @@ function CategoryPage(props){
     const [bucketList, setBucketList] = useState([])
     const [category, setCategory] = useState(props.route.params.category)
     const [content2, setContent] = useState(props.route.params.data)
-
+    const [count, changeCount] = useState()
     useFocusEffect(
         React.useCallback(() => {
             console.log('Screen was focused');
@@ -48,7 +48,7 @@ function CategoryPage(props){
                     console.log('자 축소하자',response.request._response);
                     const parsedResponse = JSON.parse(response.request._response);
                     const bucketImageList = parsedResponse.data.map(item => item.bucketImage);
-                    console.log(bucketImageList);
+                    console.log('asdfasdfsadf',bucketImageList);
                     changeimage(bucketImageList)
 
                     const bucketTitle = parsedResponse.data.map(item => item.title);
@@ -59,7 +59,7 @@ function CategoryPage(props){
                     setBucketList(newItems);
                     console.log(Math.ceil(newItems.length / 2));
                     setRows(Math.ceil(newItems.length / 2));
-
+                    changeCount(Math.ceil(newItems.length / 2));
                 })
                 .catch((error) => {
                     console.log('버킷 발급 실패');
@@ -86,50 +86,59 @@ function CategoryPage(props){
             <Text style={{fontSize: 20, color: '#FF357E'}}>"{props.route.params.data[1]}"의 버킷</Text>
 
             <View style ={{ width : '2500%', height : '70%', alignItems : 'center', margin : 3}}>
-                <ScrollView>
-                    <View style={{ flexDirection: 'column' }}>
-                        {Array.from(Array(rows)).map((_, rowIndex) => (
-                            <View style={{ flexDirection: 'row' }} key={rowIndex}>
-                                {Array.from(Array(2)).map((_, colIndex) => {
-                                    const index = rowIndex * 2 + colIndex;
-                                    if (index < bucketList.length) {
-                                        const content = bucketList[index];
-                                        console.log(content);
-                                        return (
-                                            <TouchableOpacity
-                                                onPress={() => {
-                                                    props.navigation.navigate('BucketDetail', {data : bucket_title[index], id : content[0], categoryId : content2[2], modify: true});
-                                                }}
-                                                key={index}
-                                            >
-                                                <View>
-                                                    <View style={{ flexDirection: 'column', alignItems: 'center',
-                                                        margin: 10, backgroundColor: 'white', borderRadius: 0, borderColor: 'black',
-                                                        borderWidth: 1,}}>
-                                                        <Text style={{fontSize:17, textAlign: 'center', color: 'black'}}>{content[2]}</Text>
-                                                        <Image
-                                                            style={{
-                                                                width: 160,
-                                                                height: 140,
-                                                                borderColor: '#FFECEC',
-                                                                borderWidth: 2,
-                                                                flexDirection: 'row',
-                                                                borderRadius: 0,
-                                                            }}
-                                                            source={{ uri: content[1] }}
-                                                        />
-                                                    </View>
-                                                </View>
-                                            </TouchableOpacity>
-                                        );
-                                    } else {
-                                        return <View style={{ flex: 0.5 }} key={index} />;
-                                    }
-                                })}
+                {
+                    count !== 0 ? (
+                        <ScrollView>
+                            <View style={{ flexDirection: 'column' }}>
+                                {Array.from(Array(rows)).map((_, rowIndex) => (
+                                    <View style={{ flexDirection: 'row' }} key={rowIndex}>
+                                        {Array.from(Array(2)).map((_, colIndex) => {
+                                            const index = rowIndex * 2 + colIndex;
+                                            if (index < bucketList.length) {
+                                                const content = bucketList[index];
+                                                console.log(content);
+                                                return (
+                                                    <TouchableOpacity
+                                                        onPress={() => {
+                                                            props.navigation.navigate('BucketDetail', {data : bucket_title[index], id : content[0], categoryId : content2[2], modify: true});
+                                                        }}
+                                                        key={index}
+                                                    >
+                                                        <View>
+                                                            <View style={{ flexDirection: 'column', alignItems: 'center',
+                                                                margin: 10, backgroundColor: 'white', borderRadius: 0, borderColor: 'black',
+                                                                borderWidth: 1,}}>
+                                                                <Text style={{fontSize:17, textAlign: 'center', color: 'black'}}>{content[2]}</Text>
+                                                                <Image
+                                                                    style={{
+                                                                        width: 160,
+                                                                        height: 140,
+                                                                        borderColor: '#FFECEC',
+                                                                        borderWidth: 2,
+                                                                        flexDirection: 'row',
+                                                                        borderRadius: 0,
+                                                                    }}
+                                                                    source={{ uri: content[1] }}
+                                                                />
+                                                            </View>
+                                                        </View>
+                                                    </TouchableOpacity>
+                                                );
+                                            } else {
+                                                return <View style={{ flex: 0.5 }} key={index} />;
+                                            }
+                                        })}
+                                    </View>
+                                ))}
                             </View>
-                        ))}
-                    </View>
-                </ScrollView>
+                        </ScrollView>
+                    ) : (
+                        <Image
+                            source={require('./../../img/김이영.png')}
+                            style={{ width: 280, height: 270}}
+                        />
+                    )
+                }
             </View>
 
             <View style={styles.bottomView}>
